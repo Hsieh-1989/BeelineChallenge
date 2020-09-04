9f6aa3c157b16389d74126484a04357c7ca8a0fd
Hsieh <mr.hsieh1989@gmail.com>
2020-09-04 10:20:19 +0800

    initial project

---
+++ BeelineChellenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeelineChellenge
+//
+//  Created by Hsieh on 2020/9/4.
+//  Copyright © 2020 Hsieh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
